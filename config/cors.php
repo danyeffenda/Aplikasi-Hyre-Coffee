9d4788489f7cf630e09eac3fa0dc1fee29175cab
unknown <danyeffenda@gmail.com>
2026-06-29 15:03:51 +0700

    ubah cors allowed origins

--- config/cors.php
+++ config/cors.php
@@ -9,6 +9,7 @@ return [
     'allowed_origins' => [
         'http://localhost:5173',
         'http://127.0.0.1:5173',
+        'http://effendaproject.my.id'    
     ],
 
     'allowed_origins_patterns' => [],
